8d926397a74138cd0f780150987267491d4b6a22
Lucas Nascimento <lnascimento96@yahoo.com.br>
2017-05-14 20:40:06 -0300

    Primeiro commit

---
+++ src/main/java/com/lucasnascimento/festa/controller/ConvidadosController.java
@@ -0,0 +1,28 @@
+package com.lucasnascimento.festa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lucasnascimento.festa.repository.Convidados;
+
+@Controller
+public class ConvidadosController {
+	
+	@Autowired
+	private Convidados convidados;
+	
+	
+	@GetMapping("/convidados")
+	public ModelAndView listar (){
+		
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		
+		modelAndView.addObject("convidados",convidados.findAll());
+		
+		return modelAndView;
+		
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into convidado (id, nome, quantidade_acompanhantes) values (1, 'Pedro',4);
+insert into convidado (id, nome, quantidade_acompanhantes) values (2, 'Maria',2);
+insert into convidado (id, nome, quantidade_acompanhantes) values (3, 'Ricardo',5);
